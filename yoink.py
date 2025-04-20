7e61f766dd777778866a09dcab9154f640d1e4a9
ALI JAMIL <alijamil@outlook.com.tr>
2025-04-20 16:45:31 +0300

    Fuck You Apple

---
+++ yoink.py
@@ -0,0 +1,20 @@
+from manim import *
+
+class Yoink(Scene):
+    def construct(self):
+        yoink_text = Text("Yoink").scale(5)
+
+        yoink_text.set_color_by_gradient(RED, BLUE, GREEN, YELLOW, ORANGE, PURPLE)
+        self.play(Write(yoink_text))   
+
+        siuu = Text("Siuu").scale(5).next_to(yoink_text, DOWN, buff=0.5)
+        siuu.set_color_by_gradient(RED, BLUE, GREEN, YELLOW, ORANGE, PURPLE)
+        self.play(Write(siuu))
+
+
+class FuckYou(Scene):
+    def construct(self):
+        Fuck_You = Text("Fuck You Apple").scale(2)
+
+        Fuck_You.set_color_by_gradient(RED, BLUE, GREEN, YELLOW, ORANGE, PURPLE)
+        self.play(Write(Fuck_You))
